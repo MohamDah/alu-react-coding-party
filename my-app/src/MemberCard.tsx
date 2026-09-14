b0a385e2d474e163afb4dc5652a57859c3b5ed37
Esther447 <esther@example.com>
2026-09-14 11:25:01 +0200

    MembercardProps interface

--- my-app/src/MemberCard.tsx
+++ my-app/src/MemberCard.tsx
@@ -1,8 +1,17 @@
 import type React from "react"
 
+// Author: Esther Mushimiyimana
+interface MemberCardProps {
+    name: string
+    role: string
+}
+
 // Author: Mohamed Dahab
-const MemberCard: React.FC = () => {
-    return <div>Member Card</div>
+const MemberCard: React.FC<MemberCardProps> = ({name, role}) => {
+    return <div>
+        <h1>Name: {name}</h1>
+        <h2>Role: {role}</h2>
+    </div>
 }
 
 export default MemberCard
