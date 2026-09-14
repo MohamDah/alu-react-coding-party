fc95144b771258b5568dbf952ef213c362af74e9
Erica015 <e.ishimwe2@alustudent.com>
2026-09-14 22:31:26 +1300

    added a second button for decrease

--- my-app/src/TeamDashboard.tsx
+++ my-app/src/TeamDashboard.tsx
@@ -1,41 +1,58 @@
 // Author: Mohamed Dahab
 
-import React, { useState } from "react";;
+import React, { useState } from "react";
 import MemberCard from "./MemberCard";
 
 const TeamDashboard: React.FC = () => {
 
-    //Author: Erica Ishimwe
+    // Author: Erica Ishimwe 
     const [teamScore, setTeamScore] = useState<number>(0);
 
-    return <>
+    return (
+        <>
+            <h1>Team Dashboard Component</h1>
 
-        <h1>Team Dashboard Component</h1>
+            {/* Author: Erica Ishimwe */}
+            <p>Team Score: {teamScore}</p>
 
-        {/* Author: Erica Ishimwe */}
-        <p>Team Score: {teamScore}</p>
+            {/* Author: Erica Ishimwe /}
+            <button
+                onClick={() =>
+                    setTeamScore((currentScore) => currentScore + 1)
+                }
+            >
+                Increase Score
+            </button>
 
-        {/* Author: Erica Ishimwe  */}
-<button onClick={() => setTeamScore((currentScore) => currentScore + 1)}>
-    Increase Score
-</button>
+            {/* Author: Erica Ishimwe - Task 35 */}
+            <button
+                onClick={() =>
+                    setTeamScore((currentScore) =>
+                        Math.max(0, currentScore - 1)
+                    )
+                }
+            >
+                Decrease Score
+            </button>
 
-        {/* Author :Kadi Matou Koita */}
-        {/* Autor : Ineza Bonae : added task completed to the member card component */}
-        <MemberCard
-    name="Kadi Koita"
-    role="Backend Developer"
-    taskCompleted={5}
-    isActive={true}
-/>
+            {/* Author :Kadi Matou Koita */}
+            {/* Autor : Ineza Bonae : added task completed to the member card component */}
 
-<MemberCard
-    name="Esther"
-    role="Frontend Developer"
-    taskCompleted={3}
-    isActive={false}
-/>
-    </>
-}
+            <MemberCard
+                name="Kadi Koita"
+                role="Backend Developer"
+                taskCompleted={5}
+                isActive={true}
+            />
 
-export default TeamDashboard
+            <MemberCard
+                name="Esther"
+                role="Frontend Developer"
+                taskCompleted={3}
+                isActive={false}
+            />
+        </>
+    );
+};
+
+export default TeamDashboard;
