4c03fe670450a731d7870bfc4b31202e30cc8053
Kkadi20 <k.koita@alustudent.com>
2026-09-14 11:42:35 +0200

    fix task 13 Authorship

--- my-app/src/TeamDashboard.tsx
+++ my-app/src/TeamDashboard.tsx
@@ -6,7 +6,7 @@ import MemberCard from "./MemberCard";
 const TeamDashboard: React.FC = () => {
     return <>
         <h1>Team Dashboard Component</h1>
-        //Author :Kadi Matou Koita 
+        // Author :Kadi Matou Koita 
         <MemberCard  name="Kadi Koita" role="Backend Developer" />
         <MemberCard  name="Esther" role="Frontend Developer" />
     </>
