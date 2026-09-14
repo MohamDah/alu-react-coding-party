86105be5eb31462c2a9346df9d945075fbfffe2b
Erica015 <e.ishimwe2@alustudent.com>
2026-09-14 22:20:52 +1300

    completed 31 and 32 added and displayed teamScore state

--- my-app/src/TeamDashboard.tsx
+++ my-app/src/TeamDashboard.tsx
@@ -1,11 +1,18 @@
 // Author: Mohamed Dahab
 
-import type React from "react";
+import React, { useState } from "react";;
 import MemberCard from "./MemberCard";
 
 const TeamDashboard: React.FC = () => {
+
+    //Author: Erica Ishimwe
+    const [teamScore, setTeamScore] = useState<number>(0);
+
     return <>
+
         <h1>Team Dashboard Component</h1>
+        {/* Author: Erica Ishimwe */}
+        <p>Team Score: {teamScore}</p>
         {/* Author :Kadi Matou Koita */}
         {/* Autor : Ineza Bonae : added task completed to the member card component */}
         <MemberCard
